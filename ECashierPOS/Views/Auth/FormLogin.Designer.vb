746e355f3525df50dbd9e74216bf37f46a2c3940
Beta7x <adepriyantowidies@gmail.com>
2025-06-25 01:15:48 +0700

    [MODIFY][Beta7x] - Change the login form

--- ECashierPOS/Views/Auth/FormLogin.Designer.vb
+++ ECashierPOS/Views/Auth/FormLogin.Designer.vb
@@ -22,10 +22,44 @@ Partial Class FormLogin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        components = New ComponentModel.Container()
+        Dim CustomizableEdges3 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges4 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Guna2Elipse1 = New Guna.UI2.WinForms.Guna2Elipse(components)
+        cbClose = New Guna.UI2.WinForms.Guna2ControlBox()
+        SuspendLayout()
+        ' 
+        ' Guna2Elipse1
+        ' 
+        Guna2Elipse1.BorderRadius = 8
+        Guna2Elipse1.TargetControl = Me
+        ' 
+        ' cbClose
+        ' 
+        cbClose.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        cbClose.BorderRadius = 15
+        cbClose.CustomizableEdges = CustomizableEdges3
+        cbClose.FillColor = Color.FromArgb(CByte(139), CByte(152), CByte(166))
+        cbClose.IconColor = Color.White
+        cbClose.Location = New Point(758, 12)
+        cbClose.Name = "cbClose"
+        cbClose.ShadowDecoration.CustomizableEdges = CustomizableEdges4
+        cbClose.Size = New Size(30, 30)
+        cbClose.TabIndex = 0
+        ' 
+        ' FormLogin
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(cbClose)
+        FormBorderStyle = FormBorderStyle.None
+        Name = "FormLogin"
+        Text = "Form1"
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Guna2Elipse1 As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents cbClose As Guna.UI2.WinForms.Guna2ControlBox
 
 End Class
