6dfe3986ec1ab04cb63b2881b88ce4bea6d8c5a8
Beta7x <adepriyantowidies@gmail.com>
2025-07-08 16:20:45 +0700

    [MODIFY][Beta7x] - Add new method for search user and update user without including password

--- ECashierPOS/Models/Repositories/UserRepository.vb
+++ ECashierPOS/Models/Repositories/UserRepository.vb
@@ -1,4 +1,5 @@
 ﻿Imports ECashierPOS.Database.Factories
+Imports ECashierPOS.Helpers
 Imports ECashierPOS.Models.Entities
 Imports ECashierPOS.Models.Interfaces
 Imports ECashierPOS.Utils
@@ -56,7 +57,7 @@ Namespace Models.Repositories
 
                         Dim param = cmd.CreateParameter()
                         param.ParameterName = "@username"
-                        param.Value = "%" & username & "%"
+                        param.Value = username
                         cmd.Parameters.Add(param)
 
                         Using reader As IDataReader = cmd.ExecuteReader()
@@ -70,6 +71,65 @@ Namespace Models.Repositories
                 End Using
             Catch ex As Exception
                 Return OperationResult(Of User).Fail($"[GET BY USERNAME] Gagal mendapatkan data: {ex.Message}", ex)
+            End Try
+        End Function
+
+        Public Function SearchUser(keyword As String) As OperationResult(Of List(Of User)) Implements IUserRepository.SearchUser
+            Dim users As New List(Of User)
+            Dim sql As String =
+                $"SELECT * FROM {TableName}
+                WHERE full_name LIKE @keyword OR username LIKE @keyword OR role like @keyword ORDER BY full_name ASC"
+
+            Try
+                Using conn As IDbConnection = GetConnection()
+                    conn.Open()
+                    Using cmd As IDbCommand = conn.CreateCommand()
+                        cmd.CommandText = sql
+
+                        Dim param = cmd.CreateParameter()
+                        param.ParameterName = "@keyword"
+                        param.Value = "%" & keyword & "%"
+                        cmd.Parameters.Add(param)
+
+                        Using reader As IDataReader = cmd.ExecuteReader()
+                            While reader.Read()
+                                users.Add(MapEntity(reader))
+                            End While
+                        End Using
+                    End Using
+                End Using
+
+                If users.Count = 0 Then
+                    Return OperationResult(Of List(Of User)).Ok(Nothing, "Tidak ada data.")
+                End If
+
+                Return OperationResult(Of List(Of User)).Ok(users, "Berhasil mendapatkan data.")
+            Catch ex As Exception
+                Return OperationResult(Of List(Of User)).Fail($"[SEARCH] Gagal mendapatkan data: {ex.Message}", ex)
+            End Try
+        End Function
+
+        Public Function UpdateWithoutPassword(entity As User) As OperationResult Implements IUserRepository.UpdateWithoutPassword
+            Try
+                Using conn As IDbConnection = GetConnection()
+                    conn.Open()
+                    Dim fields = New Dictionary(Of String, Object) From {
+                        {"id", entity.Id},
+                        {"full_name", entity.FullName},
+                        {"username", entity.Username},
+                        {"role", entity.Role}
+                    }
+                    Dim cmd = SqlHelper.CreateUpdateCommand(TableName, fields, KeyField, entity.Id, conn,
+                                                            excludedFields:=New List(Of String) From {KeyField})
+
+                    If cmd.ExecuteNonQuery() > 0 Then
+                        Return OperationResult.Ok("Berhasil memperbarui data.")
+                    End If
+
+                    Return OperationResult.Ok("Data sudah diperbarui.")
+                End Using
+            Catch ex As Exception
+                Return OperationResult.Fail($"[UPDATE] Gagal memperbarui data: {ex.Message}", ex)
             End Try
         End Function
     End Class
