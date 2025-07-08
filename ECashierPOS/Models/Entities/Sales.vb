436cf15656e19a40d623a97e45c7129e9dd86266
Beta7x <adepriyantowidies@gmail.com>
2025-07-08 16:11:11 +0700

    [ADD][Beta7x] - Add new entity file Sales and SalesItems

---
+++ ECashierPOS/Models/Entities/Sales.vb
@@ -0,0 +1,16 @@
+﻿Namespace Models.Entities
+    Public Class Sales
+        Implements IEntity
+
+        Public Property Id As String Implements IEntity.Id
+        'Public Property InvoiceNumber As String
+        Public Property CashierId As String
+        Public Property CustomerId As String
+        Public Property TotalPrice As Decimal
+        Public Property Discount As Decimal
+        Public Property GrandTotal As Decimal
+        Public Property Payment As Decimal
+        Public Property Change As Decimal
+        Public Property CreatedAt As DateTime
+    End Class
+End Namespace
---
+++ ECashierPOS/Models/Entities/SalesItems.vb
@@ -0,0 +1,15 @@
+﻿Namespace Models.Entities
+    Public Class SalesItems
+        Implements IEntity
+
+        Public Property Id As String Implements IEntity.Id
+        Public Property SalesId As String
+        Public Property ProductId As String
+        Public Property ProductName As String ' Join table
+        Public Property ProductImage As String ' Join table
+        Public Property Price As Decimal
+        Public Property Quantity As Integer
+        Public Property TotalPrice As Decimal
+        Public Property CreatedAt As DateTime
+    End Class
+End Namespace
