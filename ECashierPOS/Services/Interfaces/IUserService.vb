544e6645e28697b782299e8d5c04984f9ab905a4
Beta7x <adepriyantowidies@gmail.com>
2025-07-08 16:16:50 +0700

    [ADD][Beta7x] - Add new service file for user

---
+++ ECashierPOS/Services/Interfaces/IUserService.vb
@@ -0,0 +1,14 @@
+﻿Imports ECashierPOS.Models.DTO
+Imports ECashierPOS.Models.Entities
+Imports ECashierPOS.Utils
+
+Namespace Services.Interfaces
+    Public Interface IUserService
+        Function AddUser(createUserRequest As UserDTO) As OperationResult
+        Function UpdateUser(updateUserRequest As UserDTO) As OperationResult
+        Function DeleteUser(userId As String) As OperationResult
+        Function GetUserById(userId As String) As OperationResult(Of User)
+        Function GetAllUsers() As OperationResult(Of List(Of User))
+        Function SearchUser(keyword As String) As OperationResult(Of List(Of User))
+    End Interface
+End Namespace
