557eecf4f93e6f6e7e8bcc2c81c9929c5743d478
Beta7x <adepriyantowidies@gmail.com>
2025-07-08 16:12:51 +0700

    [MODIFY][Beta7x] - Add some method for search and update without including password

--- ECashierPOS/Models/Interfaces/IUserRepository.vb
+++ ECashierPOS/Models/Interfaces/IUserRepository.vb
@@ -6,5 +6,7 @@ Namespace Models.Interfaces
         Inherits IRepository(Of User)
 
         Function GetByUsername(username As String) As OperationResult(Of User)
+        Function SearchUser(keyword As String) As OperationResult(Of List(Of User))
+        Function UpdateWithoutPassword(entity As User) As OperationResult
     End Interface
 End Namespace
